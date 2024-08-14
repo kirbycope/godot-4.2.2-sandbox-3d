0c7a7e69f8cfb8484414224ff4197c0f958935c8
Timothy Cope <kirbycope@gmail.com>
2024-08-13 18:18:44 -0700

    working portal

---
+++ scripts/minecraft/portal.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite3D.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_3d_body_entered(body):
+	if body is CharacterBody3D:
+		# Switch to the specified scene 
+		get_tree().change_scene_to_file("res://scenes/minecraft/minecraft.tscn")
